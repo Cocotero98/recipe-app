dfc1266f120fd881b8ef2b70b15e8dea0b5c086d
Cocotero98 <aguilarag98@gmail.com>
2023-01-17 09:42:47 -0300

    recipe list and details added

---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.module';
+
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[]= [
+    new Ingredient('apples', 5),
+    new Ingredient('tomattos', 10),
+  ];
+
+  constructor () {
+
+  }
+
+  ngOnInit() {
+    
+  }
+}
